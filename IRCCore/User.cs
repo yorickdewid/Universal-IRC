975d206f7bb0b38a1aa1430c54131bc26e08c446
Yorick de Wid <yorick17@outlook.com>
2018-12-29 17:19:17 +0100

    IRC user and authentication options

---
+++ IRCCore/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalIRC.IRCCore
+{
+    /// <summary>
+    /// Represents an IRC user.
+    /// </summary>
+    public class User
+    {
+        public string UserName { get; set; }
+        public string RealName { get; set; }
+        public string NickName { get; set; }
+        public string NickNameSecond { get; set; }
+        public string NickNameThird { get; set; }
+        public string Password { get; set; }
+        UserAuthenticationMethod LoginMethod { get; set; } = UserAuthenticationMethod.None;
+
+        public User(string userName)
+        {
+            UserName = userName;
+            RealName = userName;
+            NickName = userName;
+        }
+
+        public User(string userName, string password, UserAuthenticationMethod loginMethod = UserAuthenticationMethod.ServerPassword)
+        {
+            UserName = userName;
+            RealName = userName;
+            NickName = userName;
+            Password = password;
+            LoginMethod = LoginMethod;
+        }
+
+        public override string ToString()
+        {
+            return UserName;
+        }
+    }
+}
