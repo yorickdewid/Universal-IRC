9bc7b66b31b7f16cb2d8057c3e6076be1874736b
Yorick de Wid <yorick17@outlook.com>
2018-12-29 17:18:52 +0100

    IRC network object

---
+++ IRCCore/Network.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalIRC.IRCCore
+{
+    public class Network
+    {
+        public string Name { get; set; }
+        public string Host { get; }
+        public int Port { get; set; } = 6667;
+        public bool UseSSL { get; set; } = true;
+        public bool AcceptInvalidCertificate { get; set; } = false;
+        public User User { get; set; }
+        public bool IsAnonymous { get => User == null; }
+        public bool KeepAlive { get; set; } = true;
+
+        /// <summary>
+        /// Create a remote network instance.
+        /// </summary>
+        /// <param name="host">Server hostname.</param>
+        public Network(string host)
+        {
+            Host = host;
+        }
+
+        /// <summary>
+        /// Create a remote network instance.
+        /// </summary>
+        /// <param name="host">Server hostname.</param>
+        /// <param name="port">Server port.</param>
+        public Network(string host, int port)
+        {
+            Host = host;
+            Port = port;
+        }
+
+        /// <summary>
+        /// Create a remote network instance.
+        /// </summary>
+        /// <param name="host">Server hostname.</param>
+        /// <param name="port">Server port.</param>
+        /// <param name="name">Network name.</param>
+        public Network(string host, int port, string name)
+        {
+            Host = host;
+            Port = port;
+            Name = name;
+        }
+
+        /// <summary>
+        /// Create a remote network instance.
+        /// </summary>
+        /// <param name="host">Server hostname.</param>
+        /// <param name="port">Server port.</param>
+        /// <param name="user">Authentication object.</param>
+        public Network(string host, int port, User user)
+        {
+            Host = host;
+            Port = port;
+            User = user;
+        }
+
+        /// <summary>
+        /// Create a remote network instance.
+        /// </summary>
+        /// <param name="host">Server hostname.</param>
+        /// <param name="port">Server port.</param>
+        /// <param name="user">Authentication object.</param>
+        /// <param name="name">Network name.</param>
+        public Network(string host, int port, User user, string name)
+        {
+            Host = host;
+            Port = port;
+            User = user;
+            Name = name;
+        }
+    }
+}
