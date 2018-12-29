4a83eab19f3164dadb47c538f64d94664e3f52e1
Yorick de Wid <yorick17@outlook.com>
2018-12-29 17:23:01 +0100

    Guest user based on User

---
+++ IRCCore/GuestUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalIRC.IRCCore
+{
+    public class GuestUser : User
+    {
+        private static string GenerateRandomName()
+        {
+            var counter = 4861;
+            return $"Guest{counter}";
+        }
+
+        public GuestUser()
+            : base(GenerateRandomName())
+        {
+        }
+    }
+}
