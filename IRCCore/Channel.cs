f0b9757df67a6a73fda9ed97b9c8f46211df86d8
Yorick de Wid <yorick17@outlook.com>
2018-12-30 15:22:46 +0100

    IRC Channel object

---
+++ IRCCore/Channel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalIRC.IRCCore
+{
+    /// <summary>
+    /// IRC channel.
+    /// </summary>
+    public class Channel
+    {
+        public string Name { get; }
+
+        public Channel(string name)
+        {
+            Name = name;
+        }
+    }
+}
