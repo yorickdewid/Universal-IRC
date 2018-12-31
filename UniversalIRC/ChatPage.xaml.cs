147efac31152bde98d86421082afe973d48ed715
Yorick de Wid <yorick17@outlook.com>
2018-12-31 15:46:43 +0100

    Basic UWP layout

---
+++ UniversalIRC/ChatPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UniversalIRC
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ChatPage : Page
+    {
+        public ChatPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        public void KeyboardKey_Pressed(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                SendMessage();
+            }
+        }
+
+        private async void SendMessage()
+        {
+            if (MessageTextBox.Text.Length != 0)
+            {
+                ConversationList.Items.Add($"{MessageTextBox.Text}");
+                MessageTextBox.Text = string.Empty;
+            }
+        }
+    }
+}
