5e92abda1c8dca34fdeac14977ce01a5489417ea
Yorick de Wid <yorick17@outlook.com>
2019-03-29 17:44:04 +0100

    Add extended splash screen

---
+++ UniversalIRC/ViewModels/ExtendedSplashScreenViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+using Microsoft.Toolkit.Uwp.UI.Animations;
+
+using UniversalIRC.Core.Models;
+using UniversalIRC.Core.Services;
+using UniversalIRC.Helpers;
+using UniversalIRC.Services;
+using UniversalIRC.Views;
+
+using Windows.UI.Xaml.Controls;
+
+namespace UniversalIRC.ViewModels
+{
+    public class ExtendedSplashScreenViewModel : Observable
+    {
+        public ExtendedSplashScreenViewModel()
+        {
+        }
+
+        private void OnsItemSelected(ItemClickEventArgs args)
+        {
+            //NavigationService.Navigate<ExtendedSplashScreenDetailPage>(selected.ID);
+        }
+    }
+}
---
+++ UniversalIRC/Views/ExtendedSplashScreenPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using UniversalIRC.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace UniversalIRC.Views
+{
+    public sealed partial class ExtendedSplashScreenPage : Page
+    {
+        public ExtendedSplashScreenViewModel ViewModel { get; } = new ExtendedSplashScreenViewModel();
+
+        public ExtendedSplashScreenPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
