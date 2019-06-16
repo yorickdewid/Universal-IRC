ce6bd570f088d05bddd852a69128a4f054125b6e
Yorick de Wid <yorick17@outlook.com>
2019-06-16 15:57:33 +0200

    CTCP command

---
+++ UniversalIRC.RelayChat/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniversalIRC.RelayChat.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string AsCTime(this DateTime dateTime)
+        {
+            return dateTime.ToString("ddd, dd MMM yyy HH:mm:ss 'GMT'");
+        }
+    }
+}
---
+++ UniversalIRC.RelayChat/Protocol/Ctcp.cs
@@ -0,0 +1,26 @@
+﻿namespace UniversalIRC.RelayChat.Protocol
+{
+    public class Ctcp
+    {
+        public CtcpCommand Command { get; set; }
+        public string Message { get; set; }
+
+        public Ctcp(CtcpCommand command)
+        {
+            Command = command;
+        }
+
+        public Ctcp(CtcpCommand command, string message)
+        {
+            Command = command;
+            Message = message;
+        }
+
+        public override string ToString()
+        {
+            return !string.IsNullOrEmpty(Message)
+                ? $"\u0001{Command} {Message}\u0001"
+                : $"\u0001{Command}\u0001";
+        }
+    }
+}
