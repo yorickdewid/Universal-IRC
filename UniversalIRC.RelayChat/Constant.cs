571e579d9eb5d79af13d73dedca212409a22c2b7
Yorick de Wid <yorick17@outlook.com>
2019-06-16 15:57:17 +0200

    Application helpers

---
+++ UniversalIRC.RelayChat/Constant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace UniversalIRC.RelayChat
+{
+    public static class Constant
+    {
+        /// <summary>
+        /// Retrieve application version.
+        /// </summary>
+        public static Version ApplicationVersion => Assembly.GetExecutingAssembly().GetName().Version;
+
+        /// <summary>
+        /// Retrieve application name.
+        /// </summary>
+        public static string ApplicationName => Assembly.GetExecutingAssembly().GetName().Name;
+
+        /// <summary>
+        /// Get application version string.
+        /// </summary>
+        public static string ApplicationVersionString => $"{ApplicationName} {ApplicationVersion.ToString()}";
+    }
+}
