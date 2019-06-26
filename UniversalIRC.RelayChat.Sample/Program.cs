ebea844dd54f062ec64b46d968bdbc5519144348
Yorick de Wid <yorick17@outlook.com>
2019-06-26 17:29:03 +0200

    Run the application in hosted console

--- UniversalIRC.RelayChat.Sample/Program.cs
+++ UniversalIRC.RelayChat.Sample/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace UniversalIRC.RelayChat.Sample
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Application entry point.
+        /// </summary>
+        /// <param name="args">Commandline arguments.</param>
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<ChatHostedService>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            await host.RunAsync();
         }
     }
 }
