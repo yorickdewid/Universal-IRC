42b6ca3fcfedc2b6453851c6b23df38ce57a1ffa
Yorick de Wid <yorick17@outlook.com>
2019-11-17 14:12:32 +0100

    Setup for terminal UI client

---
+++ UniversalIRC.RelayChat.TerminalUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniversalIRC.RelayChat.TerminalUI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
