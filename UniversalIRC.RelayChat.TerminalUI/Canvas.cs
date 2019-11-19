a34a5b25741b46c4b520cc68a89b95cf8b54b328
Yorick de Wid <yorick17@outlook.com>
2019-11-19 23:51:07 +0100

    Move all into canvas

---
+++ UniversalIRC.RelayChat.TerminalUI/Canvas.cs
@@ -0,0 +1,65 @@
+﻿using Terminal.Gui;
+
+namespace UniversalIRC.RelayChat.TerminalUI
+{
+    internal class Canvas : View
+    {
+        private View currentView;
+
+        public Canvas()
+        {
+            currentView = new ConnectWindow();
+
+            AddLayoutView();
+            AddLayoutAction();
+        }
+
+        public Canvas(Rect frame)
+            : base(frame)
+        {
+            currentView = new ConnectWindow();
+
+            AddLayoutView();
+            AddLayoutAction();
+        }
+
+        protected virtual void AddLayoutView()
+        {
+            var menu = new MenuBar(new MenuBarItem[] {
+                new MenuBarItem ("_File", new MenuItem [] {
+                    new MenuItem ("_Quit", string.Empty, SignalQuit),
+                }),
+                new MenuBarItem ("_Channel", new MenuItem [] {
+                    new MenuItem ("_Channel list", string.Empty, null),
+                    new MenuItem ("_Join", string.Empty, null),
+                    new MenuItem ("_Part", string.Empty, null),
+                }),
+                new MenuBarItem ("_Help", new MenuItem [] {
+                    new MenuItem ("_About", string.Empty, () => MessageBox.Query(50, 7, "About", "Univeral IRC Chat Terminal UI", "Ok")),
+                }),
+            });
+
+            Add(menu);
+
+            currentView.X = Pos.X(menu);
+            currentView.Y = Pos.Bottom(menu);
+            currentView.Width = Dim.Fill();
+            currentView.Height = Dim.Fill();
+
+            Add(currentView);
+        }
+
+        protected virtual void AddLayoutAction()
+        {
+            //
+        }
+
+        private void SignalQuit()
+        {
+            if (MessageBox.Query(80, 7, "Quit", "Are you sure you want to quit and part all active conversations?", "Ok", "Cancel") == 0)
+            {
+                Application.RequestStop();
+            }
+        }
+    }
+}
--- UniversalIRC.RelayChat.TerminalUI/Program.cs
+++ UniversalIRC.RelayChat.TerminalUI/Program.cs
@@ -8,40 +8,17 @@ namespace UniversalIRC.RelayChat.TerminalUI
         {
             Application.Init();
 
-            SetupTopView(Application.Top);
-
-            var top = Application.Top;
-
-            Window currentWindow = new Window(string.Empty);
-
-            var connectWindow = new ConnectWindow
-            {
-                X = 0,
-                Y = 1,
-
-                // By using Dim.Fill(), it will automatically resize without manual intervention
-                Width = Dim.Fill(),
-                Height = Dim.Fill(),
-            };
-
-            var chatWindow = new ChatWindow("#channel") // connectWindow.Frame, 
-            {
-                X = 0,
-                Y = 1,
-
-                // By using Dim.Fill(), it will automatically resize without manual intervention
-                Width = Dim.Fill(),
-                Height = Dim.Fill()
-            };
-
+            SetupCanvas(Application.Top);
+#if _x
             connectWindow.Connect = (network, nick) =>
             {
                 //MessageBox.Query(60, 7, string.Empty, $"Connecting {network}", "Ok");
 
-                chatWindow.Bounds = currentWindow.Bounds;
+                chatWindow.Frame = currentWindow.Frame;
                 top.Remove(currentWindow);
                 currentWindow = chatWindow;
                 top.Add(currentWindow);
+                currentWindow.Redraw(currentWindow.Frame);
             };
 
             #region FUTURE
@@ -58,38 +35,21 @@ namespace UniversalIRC.RelayChat.TerminalUI
             //    }
             //};
             #endregion
-
-            currentWindow = connectWindow;
-            top.Add(currentWindow);
-
+#endif
             Application.Run();
         }
 
-        private static void SetupTopView(View view)
+        private static void SetupCanvas(View view)
         {
-            var menu = new MenuBar(new MenuBarItem[] {
-                new MenuBarItem ("_File", new MenuItem [] {
-                    new MenuItem ("_Quit", "", UiSignalQuit),
-                }),
-                new MenuBarItem ("_Channel", new MenuItem [] {
-                    new MenuItem ("_Channel list", "", null),
-                    new MenuItem ("_Join", "", null),
-                    new MenuItem ("_Part", "", null),
-                }),
-                new MenuBarItem ("_Help", new MenuItem [] {
-                    new MenuItem ("_About", "", () => MessageBox.Query(50, 7, "About", "Univeral IRC Chat Terminal UI", "Ok")),
-                }),
-            });
-
-            view.Add(menu);
-        }
-
-        private static void UiSignalQuit()
-        {
-            if (MessageBox.Query(80, 7, "Quit", "Are you sure you want to quit and part all active conversations?", "Ok", "Cancel") == 0)
+            var canvas = new Canvas
             {
-                Application.RequestStop();
-            }
+                X = 0,
+                Y = 0, // Pos.Top(view) + 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill(),
+            };
+
+            view.Add(canvas);
         }
     }
 }
