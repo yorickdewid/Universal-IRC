a02ae9584709a917f02265a67be05c1021b9150a
Yorick de Wid <yorickdewid@users.noreply.github.com>
2020-01-12 15:42:00 +0100

    Refactor some code

--- UniversalIRC/Activation/ActivationHandler.cs
+++ UniversalIRC/Activation/ActivationHandler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace UniversalIRC.Activation
 {
@@ -11,22 +10,22 @@ namespace UniversalIRC.Activation
         public abstract Task HandleAsync(object args);
     }
 
-    internal abstract class ActivationHandler<T> : ActivationHandler
-        where T : class
+    internal abstract class ActivationHandler<TEventArgs> : ActivationHandler
+        where TEventArgs : class
     {
-        protected abstract Task HandleInternalAsync(T args);
+        protected abstract Task HandleInternalAsync(TEventArgs args);
 
         public override async Task HandleAsync(object args)
         {
-            await HandleInternalAsync(args as T);
+            await HandleInternalAsync(args as TEventArgs);
         }
 
         public override bool CanHandle(object args)
         {
-            return args is T && CanHandleInternal(args as T);
+            return args is TEventArgs && CanHandleInternal(args as TEventArgs);
         }
 
-        protected virtual bool CanHandleInternal(T args)
+        protected virtual bool CanHandleInternal(TEventArgs args)
         {
             return true;
         }
